c9e93e18238a05e0eb468c34b8936aed3afa0829
LexaKater <125663812+LexaKater@users.noreply.github.com>
2024-04-18 19:15:38 +0300

    Add files via upload

---
+++ Scripts/Wallet.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+public class Wallet : MonoBehaviour
+{
+    public int CountCoin { get; private set; } = 0;
+
+    public event Action AmountChenged;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Coin coin))
+        {
+            ++CountCoin;
+            AmountChenged?.Invoke();
+        }
+    }
+}
